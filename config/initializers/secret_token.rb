20b29a0f89cd0cf914e8b3b03dc2f4b79175ca88
Prasanth K. Rajan <spuul@Spuuls-MacBook-Air-2.local>
2018-11-09 17:53:13 +0000

    initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HacktrainApi::Application.config.secret_key_base = 'cba5bafaf22bb52c26367f7c3851fc190e34679dd7fbf68dc08dccd319f6aecd43c4a67abca68278edcf2004ceef5b1e6093a0766a3aaa848f33f93d529e87ca'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HacktrainApi::Application.config.session_store :cookie_store, key: '_hacktrain-api_session'
